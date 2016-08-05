9166b64c54ea3443a149219f56417817c7bc9119
Casey Reed <casey.w.reed@gmail.com>
2016-08-05 11:35:52 -0500

    Pre ping pong. Fleshing out separateProducts functions

--- acme.js
+++ acme.js
@@ -1,31 +1,34 @@
 var AcmeStore = (function(acmestore){
 
-  acmestore.pickCategory = function (categories, types, products) {
+    acmestore.pickCategory = function (categories, types, products) {
 
-    console.log("pickCategory is running!")
-    var userOptionSelection = document.getElementById("optionBar").value
-    var selectedProducts = []
+        var userOptionSelection = document.getElementById("optionBar").value
+        var selectedProducts = []
 
-    if (userOptionSelection === "fireworks") {
-        console.log("fireworks is selected")
-        for (key in products) {
-            if (products[key].type === 0) {
-                selectedProducts.push(products[key])
+        if (userOptionSelection === "fireworks") {
+            console.log("fireworks is selected")
+            for (key in products) {
+                if (products[key].type === 0) {
+                    selectedProducts.push(products[key])
+                }
             }
+            acmestore.fireworksSelection(categories, types, selectedProducts)
         }
-    }
-    if (userOptionSelection === "demolition") {
-        console.log("demolition is selected")
-        for (key in products) {
-            if (products[key].type === 1) {
-                selectedProducts.push(products[key])
+        if (userOptionSelection === "demolition") {
+            console.log("demolition is selected")
+            for (key in products) {
+                if (products[key].type === 1) {
+                    selectedProducts.push(products[key])
+                }
             }
+            acmestore.demolitionsSelection(categories, types, selectedProducts)
         }
-    }
-        console.log(selectedProducts)
-        return selectedProducts
-    }
+            console.log(selectedProducts)
+            return selectedProducts
+        },
+        //Renders items to the DOM
+        acmestore.renderItems = function () {}
 
- return acmestore
+        return acmestore
 
 })(AcmeStore || {})
--- separate.js
+++ separate.js
@@ -1,5 +1,10 @@
 var AcmeStore = (function(acmestore){
 
+    acmestore.fireworksSelection = function (categories, types, selectedProducts) {
+        console.log(selectedProducts)
+    }
+
+    acmestore.demolitionsSelection = function (categories, types, selectedProducts) {}
 
     return acmestore
 
